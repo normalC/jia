76860a7bd207c74cc6b83dcac37408f223a59551
normalC <990031126@qq.com>
2019-06-11 14:53:22 +0800

    1

--- conf.py
+++ conf.py
@@ -49,8 +49,8 @@ extensions = [
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-#templates_path = ['sphinx_rtd_theme']
-templates_path = ['_templates']
+templates_path = ['sphinx_rtd_theme']
+#templates_path = ['_templates']
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 
