a0adfa4d95baafb8783461694bc68149d4230300
normalC <990031126@qq.com>
2019-06-11 14:56:17 +0800

    1

--- conf.py
+++ conf.py
@@ -56,7 +56,7 @@ templates_path = ['sphinx_rtd_theme']
 
 #http://www.sphinx-doc.org/en/stable/theming.html
 html_search_language= u'zh' #self_add
-html_logo = './image/logo/logo1.jpg' #self_add
+#html_logo = './image/logo/logo1.jpg' #self_add
 html_theme = 'haiku' #self_add
 #html_theme ='sphinxdoc'
 #html_theme = "classic"
